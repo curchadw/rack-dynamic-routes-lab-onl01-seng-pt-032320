cfa439a0ba0eba74b142635e66e873ec01998ec4
Curtis Chadwell <laurencechadwell@gmail.com>
2020-07-01 03:59:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,6 @@
 class Application
   @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
 
-<<<<<<< HEAD
   def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
@@ -21,26 +20,5 @@ class Application
           resp.finish
 
     end
-=======
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
-
-  if req.path.match(/items/)
-     item_name = req.path.split("/items/").last
-      if item = @@items.find{|item| item.name == item_name}
-        resp.write item.price
-      else
-        resp.status = 400
-        resp.write "Item not found"
-      end
-  else
-      resp.status = 404
-      resp.write "Route not found"
-  end
-    resp.finish
-
-
->>>>>>> 2a8ae2d1abe993087d2c30d2b4cbbf3f17ad645b
-
 
 end
